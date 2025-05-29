25a61b7eca4ae2974c302d2c56d5698cb1ca7bb4
Gabriel Passaes <gabriel.bc88@gmail.com>
2025-05-28 22:09:08 -0300

    chore(cors): liberado acesso CORS para origem do frontend em ambiente local

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,11 @@ import { AppModule } from './app.module.js'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  })
+
   const publicKeyPath = process.env.PUBLIC_KEY
   const privateKeyPath = process.env.PRIVATE_KEY
 
